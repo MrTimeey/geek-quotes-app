38e172069f035dfcaac117083eadc684e7b3f125
Tim Siegler <tim.siegler@fincon.eu>
2021-03-12 17:05:06 +0100

    Add static background component

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Background/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Background from "@/components/Background";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Background,
+  },
 }
 </script>
 
@@ -22,7 +21,5 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Background.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="background-wrapper">
+    <div class="background" :style="imageStyle">
+    </div>
+    <span class="author">Photo by <a href="https://unsplash.com/@headwayio?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Headway</a> on <a href="/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+  </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Background",
+  data() {
+    return {
+      image: 'https://source.unsplash.com/NWmcp5fE_4M/1920x1600',
+    }
+  },
+  computed: {
+    imageStyle() {
+      return {
+        backgroundImage: `url(${this.image})`
+      }
+    }
+
+  }
+}
+</script>
+
+<style lang="scss">
+.background-wrapper {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width:100%;
+  height: 100%;
+}
+.background {
+  width:100%;
+  height: 100%;
+  position: relative;
+  overflow: hidden;
+  background-size: cover;
+  background-repeat: no-repeat;
+}
+.author {
+  position: fixed;
+  right: 10px;
+  bottom: 10px;
+  background-color: white;
+  padding: 5px 10px 5px 10px;
+  border-radius: 10px;
+  font-size: 15px;
+  font-weight: bold;
+}
+a {
+  color: darkgrey;
+}
+</style>
